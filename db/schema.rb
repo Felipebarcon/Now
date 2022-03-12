356bd2140e7e99adab38d437da6aeb118126f241
Anais Augu <augu.anais@gmail.com>
2022-03-12 15:07:58 +0100

    solving schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_12_133556) do
+ActiveRecord::Schema.define(version: 2022_03_12_133944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "levels", force: :cascade do |t|
     t.string "name"
