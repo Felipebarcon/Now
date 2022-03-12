3f6bcee58078d57adb39e24cee93b467082200bf
Felipe Barcon <felipe.barcon@gmail.com>
2022-03-12 14:59:10 +0100

    level model + validation ok

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,4 +1,4 @@
 class Level < ApplicationRecord
   validates :name, presence: true
-  validates :frequency, numericality: { in: 1..7 }
+  validates :frequency, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 7 }
 end
