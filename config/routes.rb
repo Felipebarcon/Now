d4d8e6b9d49c7a4e61d714aeeeb1640a484b4bc2
Anais Augu <augu.anais@gmail.com>
2022-03-17 16:08:12 +0100

    summary custom route + controller methods summary and update + view first try

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :goals, only: [:new, :create]
+  resources :goals, only: [:new, :create, :update]
+  get 'goals/:id/summary', to: 'goals#summary', as: 'summary'
 end
