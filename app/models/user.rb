1e9f089ac508aa0b96003aeac692ebd5b1b76be7
Anais Augu <augu.anais@gmail.com>
2022-03-24 17:16:43 +0100

    Listing model and listings table created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_many :goals, dependent: :destroy
+
+  has_many :listings
+  has_many :events, through: :listings
+
+  has_many :events, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
