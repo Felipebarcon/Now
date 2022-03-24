29f1d9671f30f22363ad3c2c2f35f249be033575
Anais Augu <augu.anais@gmail.com>
2022-03-24 17:54:17 +0100

    validations on event about date and time

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,19 @@ class Event < ApplicationRecord
   has_many :users, through: :listings
 
   validates :description, :location, :start_date_time, :end_date_time, presence: true
+  validate :event_time_valid?
+
+  private
+
+  def event_time_valid?
+    if start_date_time < DateTime.now
+      errors.add(:start_date_time, "must be after now")
+    elsif end_date_time < DateTime.now
+      errors.add(:end_date_time, "must be after now")
+    elsif end_date_time < start_date_time
+      errors.add(:end_date_time, "must be after the start time")
+    else
+      return true
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_24_160326) do
+ActiveRecord::Schema.define(version: 2022_03_24_162920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_03_24_160326) do
     t.datetime "end_date_time", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
