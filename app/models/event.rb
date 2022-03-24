4eac555edd48a57a4b54469a549845e1fc22a736
Anais Augu <augu.anais@gmail.com>
2022-03-24 17:35:45 +0100

    validations events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :users, through: :listings
+
+  validates :description, :location, :start_date_time, :end_date_time, presence: true
 end
