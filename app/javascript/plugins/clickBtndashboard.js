3286864e6525d4008f923872ca0fdaef56ebf8f9
Felipe Barcon <felipe.barcon@gmail.com>
2022-03-26 09:48:27 +0100

    change JS button code NOK

--- app/javascript/plugins/clickBtndashboard.js
+++ app/javascript/plugins/clickBtndashboard.js
@@ -1,19 +1,20 @@
-const frequency = () => {
+
   const data = document.querySelectorAll(".card__header.frequency");
   const newArray = [];
   data.forEach((e)=> {
     newArray.push(parseInt(e.innerText, 10))
   })
-  return newArray;
-};
+
+
+
 
 const clickBtndashboard = () => {
   const button = document.querySelectorAll(".clickme");
   button.forEach((btn) => {
     let count = 0;
     btn.addEventListener('click', (e) => {
-      if (count >= frequency()) {
-        console.log("You win")
+      if (count >= newArray[0]) {
+        btn.innerHTML = "Completed!"
       } else {
         count += 1;
         btn.innerHTML = "Add a step: " + count;
