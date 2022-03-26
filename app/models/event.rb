288ff487fc3070287342bd0eba31d7de0c2830d1
Anais Augu <augu.anais@gmail.com>
2022-03-26 13:35:07 +0100

    activity added to events + base index created

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+  belongs_to :activity
   has_many :listings
   has_many :users, through: :listings
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_24_201548) do
+ActiveRecord::Schema.define(version: 2022_03_26_104410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2022_03_24_201548) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
+    t.bigint "activity_id", null: false
+    t.index ["activity_id"], name: "index_events_on_activity_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -79,6 +81,7 @@ ActiveRecord::Schema.define(version: 2022_03_24_201548) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "activities"
   add_foreign_key "events", "users"
   add_foreign_key "goals", "activities"
   add_foreign_key "goals", "levels"
