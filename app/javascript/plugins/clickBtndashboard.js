00160dcb2d4f76b1c03ed393e68dc35b033a466e
Felipe Barcon <felipe.barcon@gmail.com>
2022-03-26 12:28:29 +0100

    dashboard little changes and JS function to complete a goal

--- app/javascript/plugins/clickBtndashboard.js
+++ app/javascript/plugins/clickBtndashboard.js
@@ -1,24 +1,25 @@
-
-  const data = document.querySelectorAll(".card__header.frequency");
-  const newArray = [];
-  data.forEach((e)=> {
-    newArray.push(parseInt(e.innerText, 10))
-  })
-
-
+const data = document.querySelectorAll(".card__header.frequency");
+const newArray = [];
+data.forEach((e) => {
+  newArray.push(parseInt(e.innerText, 10))
+})
 
 
 const clickBtndashboard = () => {
   const button = document.querySelectorAll(".clickme");
   button.forEach((btn) => {
     let count = 0;
-    btn.addEventListener('click', (e) => {
-      if (count >= newArray[0]) {
-        btn.innerHTML = "Completed!"
-      } else {
-        count += 1;
-        btn.innerHTML = "Add a step: " + count;
-      }
+    newArray.forEach((frequency) => {
+      btn.addEventListener('click', (e) => {
+        console.log(newArray);
+        if (count < frequency) {
+          console.log(count);
+          count += 1;
+          btn.innerHTML = "Add a step: " + count;
+        } else {
+          btn.innerHTML = "Completed!"
+        }
+      })
     })
   })
 };
