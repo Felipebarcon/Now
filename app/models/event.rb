e9a1ce4f2fa0c58bed108d3f1497ec15abc48a36
Felipe Barcon <felipe.barcon@gmail.com>
2022-03-30 00:02:49 +0200

    new form ok, dashboard for computer ok, simplify completed js

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,7 +9,7 @@ class Event < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  validates :description, :location, :start_time, :end_time, presence: true
+  validates :description, :address, :start_time, :end_time, presence: true
   validate :event_time_valid?
 
   default_scope -> { order(:start_time) }
