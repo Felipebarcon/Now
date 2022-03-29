40e374616be6ef831aaa100e338f767d0a7464e4
Anais Augu <augu.anais@gmail.com>
2022-03-29 22:12:55 +0200

    mapbox popup style

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,7 +14,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/navigation-night-v1'
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
