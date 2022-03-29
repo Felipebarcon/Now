ae055430dbc45f2492a30fd92bc8c1c7ea8dee9a
Alexandre Da Costa Monteiro <a.dcm33@gmail.com>
2022-03-29 20:19:14 +0200

    fixed error

--- db/schema.rb
+++ db/schema.rb
@@ -66,15 +66,6 @@ ActiveRecord::Schema.define(version: 2022_03_26_101757) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "description"
-    t.bigint "goal_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.boolean "completed", default: false, null: false
-    t.index ["goal_id"], name: "index_tasks_on_goal_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -94,5 +85,4 @@ ActiveRecord::Schema.define(version: 2022_03_26_101757) do
   add_foreign_key "goals", "users"
   add_foreign_key "listings", "events"
   add_foreign_key "listings", "users"
-  add_foreign_key "tasks", "goals"
 end
