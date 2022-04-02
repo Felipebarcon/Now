539334d9d013a32fa5e02d780be2d5ddaf27ebd9
Alexandre Da Costa Monteiro <a.dcm33@gmail.com>
2022-04-02 10:52:03 +0200

    Created task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_152612) do
+ActiveRecord::Schema.define(version: 2022_04_02_084705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,15 @@ ActiveRecord::Schema.define(version: 2022_03_26_152612) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.boolean "completed"
+    t.bigint "goal_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["goal_id"], name: "index_tasks_on_goal_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -90,4 +99,5 @@ ActiveRecord::Schema.define(version: 2022_03_26_152612) do
   add_foreign_key "goals", "users"
   add_foreign_key "listings", "events"
   add_foreign_key "listings", "users"
+  add_foreign_key "tasks", "goals"
 end
