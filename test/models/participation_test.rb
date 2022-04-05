1b734705db1aa58d3361c4ebba3992c654962e51
Anais Augu <augu.anais@gmail.com>
2022-04-05 21:39:24 +0200

    button position and color + change in db schema for table registrations

--- test/models/participation_test.rb
+++ test/models/participation_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class RegistrationsControllerTest < ActionDispatch::IntegrationTest
+class ParticipationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
